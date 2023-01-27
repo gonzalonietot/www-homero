3cf890059e9f93c118b5d63119a3f3277dc044cf
gonzalonieto-soapboxx <gonzalo@soapboxx.us>
2023-01-27 18:41:14 -0300

    proyect with nuxt

---
+++ components/Presentation.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="container">
+  <img src="~/static/homero.jpg" class="rotate linear infinite" width="150" height="150" />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Presentation',
+}
+</script>
+<style>
+.container {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+.rotate {
+  animation: rotation 2s;
+}
+
+.linear {
+  animation-timing-function: linear;
+}
+
+.infinite {
+  animation-iteration-count: infinite;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Presentation />
+</template>
+
+<script>
+import Presentation from "../components/Presentation.vue";
+
+export default {
+  name: 'IndexPage',
+  components: { Presentation }
+}
+</script>
